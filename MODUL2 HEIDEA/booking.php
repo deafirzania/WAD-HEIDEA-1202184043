48e7ea642df78e3787c61ee45395e88051ee85fa
deafirzania <deafirzania@gmail.com>
2020-10-11 23:11:20 +0700

    tp 2 fix

--- MODUL2 HEIDEA/booking.php
+++ MODUL2 HEIDEA/booking.php
@@ -23,34 +23,37 @@
         </nav>
 
         <br>
-        <form action="mybooking.php" method="get">
+        <form action="mybooking.php" method="POST">
             <fieldset class="from-group">
 
                 <div class="from-group row">
                     <div class="col-md-3 ml-md-auto">
                         <div class="form-group row">
                             <label for="name">Name</label>
-                            <input type="text" class="form-control" name="nama">
+                            <input type="text" class="form-control" name="name">
                         </div>
 
                         <div class="form-group row">
-                            <label for="tanggal">Date</label>
-                            <input type="date" class="form-control" name="tgl">
+                            <label for="checkin">Date</label>
+                            <input type="date" class="form-control" name="checkin">
                         </div>
 
                         <div class="form-group row">
-                            <label for="durasi">Duration</label>
-                            <input type="text" class="form-control" name="duration">
+                            <label for="duration">Duration</label>
+                            <input type="number" class="form-control" name="duration">
                             <p style="font-size: small;">Day(s)</p>
                         </div>
                         
 
                         <div class="form-group row">
                             <label for="roomtype">Room Type</label>
-                            <select class="form-control" id="">
-                                <option>Standard</option>
-                                <option>Superior</option>
-                                <option>Luxury</option>
+                            <select class="form-control" id="" name="roomtype">
+                                <option value="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQz-87ki60ycOwyo1b-3Us3GT1kwIsxZ3jesA&usqp=CAU">
+                                Standard</option>
+                                <option value="https://www.watermark-bali.com/wp-content/uploads/2017/09/superior-room2.jpg">
+                                Superior</option>
+                                <option value="https://flh.ca/media/44731/executive-luxury_1170x650.jpg?width=848&maxwidth=848">
+                                Luxury</option>
                             </select>
                         </div>
                         Add Service(s)
@@ -77,12 +80,15 @@
                         </a>
 
                     </div>
-                    <div class="col-md-3 ml-md-auto">
-                        
+                    <div class="col-md-5 ml-md-auto">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQz-87ki60ycOwyo1b-3Us3GT1kwIsxZ3jesA&usqp=CAU">
                     </div>
                 </div>
             </fieldset>
         </form>
 
     </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </html>
--- MODUL2 HEIDEA/mybooking.php
+++ MODUL2 HEIDEA/mybooking.php
@@ -6,9 +6,32 @@
         initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        
+        <!-- <?php
+        $bookingnumber = rand();
+        $nama = $_POST['name'];
+        $checkin = $_POST['checkin'];
+        $duration = $_POST['duration'];
+        $checkout =  date('m/d/y', strtotime($check . ' + ' . $_GET['duration'] . 'days'));
+        $roomtype = $_POST['roomtype'];
+        $nohp = $_POST['nohp'];
+        $service1 = $_POST['roomservice'];
+        
+        $total;
+        if($roomtype == "standard") {
+            $total = (90*$duration)+$servicebill;
+         } elseif($roomtype == "superior"){
+             $total = (150*$duration)+$servicebill;   
+         }
+         else{
+             $total = (200*$duration)+$servicebill;
+         }
+        ?> -->
+        
         <title>
             EAD HOTEL - My Booking
         </title>
+        
     </head>
 
 
@@ -21,16 +44,7 @@
             </div>
             </div>
         </nav>
-        <?php
-        $bookingnumber = [rand()];
-        $name = $_GET['nama'];
-        $checkin = $_GET['checkin'];
-        $checkout = $_GET['checkout'];
-        $roomtype = $_GET['roomtype'];
-        $nohp = $_GET['nohp'];
-        $service = $_GET['service'];
-        $total = $_GET['total']
-        ?>
+        
         <div class="container">
             <div class="row">
                 <div class="col align-self-center">
@@ -51,12 +65,12 @@
                             <tbody>
                             <tr>
                                 <th scope="row"><? $bookingnumber ?></th>
-                                <td><?= $name ?></td>
-                                <td><?= $checkin ?></td>
-                                <td><?= $checkout ?></td>
+                                <td><?= $nama ?></td>
+                                <td><?= date('d/m/Y', strtotime($checkin)) ?></td>
+                                <td><?= date('d/m/Y', strtotime($checkout)) ?></td>
                                 <td><?= $roomtype ?></td>
                                 <td><?= $nohp ?></td>
-                                <td><?= $service ?></td>
+                                <td><ul><li><?= $service1?></li></ul></td>
                                 <td><?= $total ?></td>
                             </tr>
                         
@@ -67,4 +81,7 @@
         </div>
         
     </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </html>
